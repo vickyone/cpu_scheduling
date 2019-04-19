0a697478b0a3537c90f5806f10db6c6e89407d2e
VICKY KUMAR <49094552+vickyone@users.noreply.github.com>
2019-04-19 23:25:42 +0530

    Adding macros and names  and updating logics

--- scheduling.c
+++ scheduling.c
@@ -3,47 +3,49 @@ NAME-VICKY KUMAR
 ROLL NO-47
 SECTION-K17EX
 */
+//header files 
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/types.h>
 #include<stdlib.h>
 #include<conio.h>
+#define max 100
 
 int main()
 {
- int i,j,noprcs,bt[50],at[50],tat[50],wt[ ],temp[];
- float awt = 0, atat = 0;
+ int i,j,noprcs,burst[max],arrvt[max],turnat[max],wait[max],temp[max];
+ float await = 0, aturnat = 0;
  printf("Cpu scheduling \n");6
  printf("Enter the total number of process which we want to schedule\n");
  scanf("%d" , &noprcs);
  printf("Enter the Arrival Time of the process\n");
    for(i=0;i<n;i++)
  	{
- 		scanf("%d",&bt[i]);
+ 		scanf("%d",&arrvt[i]);
  	}
  printf("Enter the Burst Time of the process\n");
    for(i=0;i<n;i++)
  	{
- 	 	scanf("%d",&bt[i]);
+ 	 	scanf("%d",&burst[i]);
  	}
 	 temp[0] = 0;
  printf("Process \t Arrival Time \t Burst Time \n");	
 	for(i=0;i<n;i++)
 	{
-		wt[i] = 0;
-		tat[i] = 0;
-		temp[i+1] = temp[i] + bt[i];
-		wt[i] = temp[i] - at[i];
-		tat[i] = wt[i] + bt[i];
-		awt = awt + wt[i];
-		atat = atat + tat[i];
-		printf("%d \t %d \t\t\t %d\t\t%d\t\t%d",i+1,at[i],at[i],wt[i],tat[i]);	
+		wait[i] = 0;
+		turnat[i] = 0;
+		temp[i+1] = temp[i] + burst[i];
+		wait[i] = temp[i] - arrvt[i];
+		turnat[i] = wait[i] + burst[i];
+		await = await + wait[i];
+		aturnat = aturnat + turnat[i];
+		printf("%d \t %d \t\t\t %d\t\t%d\t\t%d",i+1,arrvt[i],burst[i],wait[i],turnat[i]);	
 	}
   
-  awt = awt/n;
-  atat= atat/n;
-  printf("Average Waiting Time is equal to: %f\n ", awt);
-  printf("Average Turn Around Time is equal to: %f\n ", atat);
+  await = await/n;
+  aturnat= aturnat/n;
+  printf("Average Waiting Time is equal to: %f\n ", await);
+  printf("Average Turn Around Time is equal to: %f\n ", aturnat);
    
 }
 
